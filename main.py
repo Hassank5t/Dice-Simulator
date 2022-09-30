d82be634cab5a59dea51e5bcdf568771382c8c52
Hassan Khalid <hassan.khalid@feuzion.com>
2022-10-01 01:12:53 +0500

    completed

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import random
+again = True
+while again:
+     print(random.randint(1, 6))
+     another_roll = input("want to roll the dice again? (y/n): ")
+     if another_roll.lower() == "y":
+         continue
+     else:
+          break
